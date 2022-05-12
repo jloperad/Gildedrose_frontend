e78992682d017854861fe31bdca6157d928b18cd
fsalazarr <55804707+fsalazarr@users.noreply.github.com>
2022-05-12 12:45:45 -0500

    Update proxy.config.js

--- proxy.config.js
+++ proxy.config.js
@@ -1,6 +1,6 @@
 const config = {
   "/api/": {
-    target: "http://" + process.env.BACKEND_HOST + ":8081",
+    target: "http://" + process.env.BACKEND_HOST + ":8080",
     secure: false,
     logLevel: "debug",
   },
